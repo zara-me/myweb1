4b67f91b858534b9f031c4093077653a2e77a7b4
Zara <zzz.toohy@gmail.com>
2025-11-14 02:17:34 +0300

    Update AreaChecker.java

--- Labwork1/src/main/java/ru/zara/web/AreaChecker.java
+++ Labwork1/src/main/java/ru/zara/web/AreaChecker.java
@@ -1,14 +1,14 @@
 package ru.zara.web;
 
 public class AreaChecker {
+        //  y is int
+    public static boolean isInArea(double x, int y, double r) {
 
-    public static boolean isInArea(double x, double y, double r) {
-
-        if (!Double.isFinite(x) || !Double.isFinite(y) || !Double.isFinite(r) || r <= 0) {
+        //  r < 2
+        if (!Double.isFinite(x) || r < 2 || !Double.isFinite(r)) {
             return false;
         }
 
-        // کمی تلورانس برای محاسبات ممیز شناور
         final double eps = 1e-9;
 
         // Rectangle: Q1
@@ -17,17 +17,20 @@ public class AreaChecker {
             if (x >= -eps && y >= -eps) return true;
         }
 
+        // Triangle: Q4
+        // 0 <= x <= R AND -R <= y <= 0 AND y >= x - R
         if (x >= -eps && x <= r + eps && y <= eps && y >= -r - eps) {
             double boundary = x - r; // y >= x - R
             if (y + eps >= boundary) {
-                return true;
+                // x>=0 و y<=0
+                if (x >= -eps && y <= eps) return true;
             }
         }
 
         // Quarter circle: Q3
         // x <= 0, y <= 0, and x^2 + y^2 <= R^2
         if (x <= eps && y <= eps) {
-            double lhs = x * x + y * y;
+            double lhs = x * x + (double)y * y; // y به double cast شد
             double rhs = r * r;
             if (lhs <= rhs + eps) {
                 return true;
